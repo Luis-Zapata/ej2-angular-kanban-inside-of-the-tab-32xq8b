d318f923eb8f63ce8c616cfa18cb1610826a5b9a
Luis-Zapata <luis.azapatazepeda@gmail.com>
2022-01-17 14:13:20 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ej2-angular-kanban-inside-of-the-tab-32xq8b
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ej2-angular-kanban-inside-of-the-tab-32xq8b)
---
+++ app.component.ts
@@ -0,0 +1,22 @@
+import { Component, ViewChild, ViewEncapsulation } from '@angular/core';
+import { enableRipple, createElement } from '@syncfusion/ej2-base';
+import { TabComponent } from '@syncfusion/ej2-angular-navigations';
+import { CardSettingsModel } from '@syncfusion/ej2-angular-kanban';
+import { kanbanData } from './data';
+
+enableRipple(true);
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  encapsulation: ViewEncapsulation.None
+})
+
+export class AppComponent {
+  @ViewChild('element') tabInstance: TabComponent;
+  public data: Object[] = kanbanData;
+  public cardSettings: CardSettingsModel = {
+    contentField: 'Summary',
+    headerField: 'Id'
+  };
+}
